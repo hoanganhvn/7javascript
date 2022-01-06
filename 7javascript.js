29ae7afb207371836b410cb60f939adbe46105df
{Hoàng <{nguyenhoanganhvn90@gmail.com}>
2022-01-06 17:26:02 +0700

    7javascript

---
+++ 7javascript.js
@@ -0,0 +1,20 @@
+// Start 1
+// let a = prompt("Nhập vào số bất kỳ:");
+// let b = alert("Căn bậc hai của " + a + " là: " + Math.sqrt(a).toFixed(2));
+// End 1
+
+// Start 2
+// let a = prompt("Nhập vào độ dài chiều cao tam giác:");
+// let b = prompt("Nhập vào độ dài cạnh đáy tam giác:");
+// let c = alert("Diện tích tam giác là: " + Math.round(((a*b)/2)*100/100));
+// End 2
+
+// Start 3
+// let a = prompt("Nhập vào chỉ số độ F:");
+// let b = alert("Quy đổi sang chỉ số độ C là: " + Math.ceil(((a - 32)/1.8)*100)/100);
+// End 3
+
+// Start 4
+// let a = prompt("Nhập vào số Km:");
+// let b = alert("Quy đổi sang m là: " + Math.floor((a*1000)*100)/100);
+// End 4
